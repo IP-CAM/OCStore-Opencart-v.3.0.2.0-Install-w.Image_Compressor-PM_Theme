e87dcba6ba5150d7c73931fcc74663fec60f8670
Yulms <yminsk@gmail.com>
2020-12-17 21:34:53 +0300

    Добавляет 30% header

---
+++ source_frontend/js/common-next.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+let baseHref;
+function getBaseHref() {
+  if (baseHref !== undefined)  return baseHref;
+  baseHref = document.querySelector('base').href;
+  return baseHref;
+}
+
+
+function initSearch() {
+  const FORM_SELECTOR = '.search-form';
+  const INPUT_SELECTOR = '.search-form__input';
+
+  const formElement = document.querySelectorAll(FORM_SELECTOR);
+  formElement.forEach((elem) => {
+    elem.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+
+      let url = getBaseHref() + 'index.php?route=product/search';
+      let searchValue = elem.querySelector(INPUT_SELECTOR).value;
+      if (searchValue) {
+        url += '&search=' + encodeURIComponent(searchValue);
+      }
+      window.location = url;
+    });
+  });
+}
+
+initSearch();
