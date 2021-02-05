375ecfefb342837f7fc5c7ffbc4873ca427e5c07
Yulms <yminsk@gmail.com>
2021-02-05 09:16:11 +0300

    Удаляет sourcemap из make-css.js

--- gulp_tasks/make-css.js
+++ gulp_tasks/make-css.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const sourcemap = require('gulp-sourcemaps');
+// const sourcemap = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
@@ -10,12 +10,12 @@ const rename = require('gulp-rename');
 function makeCSS (server, TEMPLATE_PATH) {
   return gulp.src('source_frontend/sass/style.scss')
     .pipe(plumber())
-    .pipe(sourcemap.init())
+    // .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(csso())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemap.write('.'))
+    // .pipe(sourcemap.write('.'))
     .pipe(gulp.dest(TEMPLATE_PATH + '/css'))
     .pipe(server.stream());
 }
