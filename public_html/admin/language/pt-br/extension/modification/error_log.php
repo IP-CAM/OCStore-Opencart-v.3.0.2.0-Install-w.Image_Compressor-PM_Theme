6bb13ec4b1d3214a0362ab313a879655886e104b
Yulms <yminsk@gmail.com>
2021-02-09 14:36:05 +0300

    Добавляет OCFilter Modification (от бразильца)

---
+++ public_html/admin/language/pt-br/extension/modification/error_log.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']      = 'Log de erros OCMOD';
+
+// Text
+$_['text_modifications'] = 'Modificações';
+$_['text_success']       = 'O log de erros OCMOD foi apagado!';
+$_['text_list']          = 'Listando o log';
+
+// Button
+$_['button_return']      = 'Voltar para modificações';
+
+// Error
+$_['error_warning']      = 'Atenção: O arquivo %s possui o tamanho de %s!';
+$_['error_empty']        = 'Atenção: O arquivo de log de erros OCMOD está vazio!';
+$_['error_permission']   = 'Atenção: Você não tem permissão para limpar o log de erros OCMOD!';
---
+++ public_html/admin/language/pt-br/extension/modification/files.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']      = 'Arquivos modificados';
+
+// Text
+$_['text_modifications'] = 'Modificações';
+$_['text_list']          = 'Listando arquivos modificados';
+$_['text_no_results']    = 'Não há arquivos modificados por OCMOD.';
+$_['text_file']          = 'Arquivo';
+$_['text_modification']  = 'Modificação';
+$_['text_version']       = 'Versão';
+$_['text_author']        = 'Autor';
+
+// Button
+$_['button_return']      = 'Voltar para modificações';
