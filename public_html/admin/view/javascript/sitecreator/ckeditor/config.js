d1ab21d891124712518c45bdb0cbe4c786d53f94
Yulms <yminsk@gmail.com>
2021-02-11 17:06:05 +0300

    Добавляет плагин подсветки синтаксиса (Codemirror) для CKEditor

--- public_html/admin/view/javascript/sitecreator/ckeditor/config.js
+++ public_html/admin/view/javascript/sitecreator/ckeditor/config.js
@@ -5,6 +5,32 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+	config.language = 'en';
 	// config.uiColor = '#AADC6E';
+
+  config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'NewPage,Cut,Copy,Paste,PasteText,PasteFromWord,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Styles';
+
+  // https://github.com/w8tcha/CKEditor-CodeMirror-Plugin
+  // https://ckeditor.com/cke4/addon/codemirror
+  config.extraPlugins = 'codemirror';
+  config.codemirror = {
+    theme: 'monokai'
+  };
 };
---
+++ public_html/admin/view/javascript/sitecreator/ckeditor/plugins/codemirror/lang/uk.js
@@ -0,0 +1,12 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'codemirror', 'uk', {
+	toolbar: 'Джерело',
+	searchCode: 'Шукати в джерелі',
+	autoFormat: 'Форматувати вибране',
+	commentSelectedRange: 'Закоментувати вибране',
+	uncommentSelectedRange: 'Розкоментувати вибране',
+	autoCompleteToggle: 'Увімкнути/Вимкнути автоматичне завершення HTML-тегів'
+});
