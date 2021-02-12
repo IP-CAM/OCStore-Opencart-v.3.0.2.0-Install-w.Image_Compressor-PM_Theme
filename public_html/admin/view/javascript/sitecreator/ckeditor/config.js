a93a9faddde660a9845c40b9c81345c408328cc2
Yulms <yminsk@gmail.com>
2021-02-12 14:10:03 +0300

    Добавляет форматирование html в CKEditor
    Добавляет незначительные изменения в CREditor config

--- public_html/admin/view/javascript/sitecreator/ckeditor/config.js
+++ public_html/admin/view/javascript/sitecreator/ckeditor/config.js
@@ -29,8 +29,156 @@ CKEDITOR.editorConfig = function( config ) {
 
   // https://github.com/w8tcha/CKEditor-CodeMirror-Plugin
   // https://ckeditor.com/cke4/addon/codemirror
-  config.extraPlugins = 'codemirror';
+  config.extraPlugins = 'codemirror, htmlwriter';
   config.codemirror = {
-    theme: 'monokai'
+    theme: 'oceanic-next',
+    enableSearchTools: false,
+    showFormatButton: false
   };
+  config.dataIndentationChars = '  ';
+  config.allowedContent = true;
+  config.height = 400;
+  // config.enterMode = CKEDITOR.ENTER_BR;
 };
+
+
+
+// Добавление возможности вставки вырезаемых тегов внутрь a
+// https://github.com/ckeditor/ckeditor4/issues/514
+const extraTagsInsideATag = {
+  picture: 1,
+  source: 1,
+  div: 1,
+  p: 1,
+  h2: 1,
+  h3: 1,
+  h4: 1,
+  h5: 1,
+  h6: 1
+};
+Object.assign(CKEDITOR.dtd['a'], extraTagsInsideATag);
+CKEDITOR.dtd.$block['a'] = 1; // делаем a блочным, для предотвращения появления лишних пробелов внутри (не проверялось)
+
+const extraTagsInsideDivPTag = {
+  picture: 1,
+  source: 1
+};
+Object.assign(CKEDITOR.dtd['div'], extraTagsInsideDivPTag);
+Object.assign(CKEDITOR.dtd['p'], extraTagsInsideDivPTag);
+
+
+// picture может содержать теги
+CKEDITOR.dtd['picture'] = {
+  source: 1,
+  img: 1
+};
+CKEDITOR.dtd.$block['picture'] = 1; // делаем блочным
+
+
+CKEDITOR.on( 'instanceReady', function( ev ) {
+  var writer = ev.editor.dataProcessor.writer;
+
+  writer.selfClosingEnd = '>';
+
+  writer.setRules( 'p', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: false,
+      breakBeforeClose: false,
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'div', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: true,
+      breakBeforeClose: true,
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'span', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: false,
+      breakBeforeClose: false,
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'a', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: true,
+      breakBeforeClose: true,
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'img', {
+      breakBeforeOpen: true
+  });
+
+  writer.setRules( 'h1', {
+      breakAfterClose: false,
+  });
+
+  writer.setRules( 'h2', {
+      breakAfterClose: false,
+  });
+
+  writer.setRules( 'h3', {
+      breakAfterClose: false,
+  });
+
+  writer.setRules( 'h4', {
+      breakAfterClose: false,
+  });
+
+  writer.setRules( 'h5', {
+      breakAfterClose: false,
+  });
+
+  writer.setRules( 'h6', {
+      breakAfterClose: false,
+  });
+
+  writer.setRules( 'ul', {
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'ol', {
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'li', {
+    indent: true,
+    breakBeforeClose: true
+});
+
+  writer.setRules( 'article', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: true,
+      breakBeforeClose: true,
+      breakAfterClose: true
+  });
+
+  writer.setRules( 'section', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: true,
+      breakBeforeClose: true,
+      breakAfterClose: true
+  });
+
+  writer.setRules( 'picture', {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: true,
+      breakBeforeClose: true,
+      breakAfterClose: false
+  });
+
+  writer.setRules( 'source', {
+      breakBeforeOpen: true
+  });
+
+});
