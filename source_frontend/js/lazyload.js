3c5f94d77da2b8a40096b5f98639accc0d7106f3
Yulms <yminsk@gmail.com>
2021-02-17 15:33:26 +0300

    Модификация скрипта lazyload

--- source_frontend/js/lazyload.js
+++ source_frontend/js/lazyload.js
@@ -1,48 +1,33 @@
 'use script';
 
-// модуль определяет поддержку loading="lazy"
-// Если есть:
-// заменяет атрибуты data-srcset и data-src на соответсвующие без data, позволяя работать нативному алгоритму
-// Если нет:
-// подключается скрипт lazysizes
-
 // https://web.dev/browser-level-image-lazy-loading/
 
-// отключить модуль при браузерной поддержке loading >85%
+
+function appendLazysizesScript() {
+  const script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js';
+  document.head.append(script);
+}
 
 
 function initLazyload() {
-  let nativeLazyloadIsEnabled;
+  let nativeLazyloadIsEnabled = false;
 
   if ('loading' in HTMLImageElement.prototype) {
-
-    const srcsetElements = document.querySelectorAll('[data-srcset]');
-    srcsetElements.forEach(srcsetElement => {
-      srcsetElement.setAttribute('srcset', srcsetElement.dataset.srcset);
-      delete srcsetElement.dataset.srcset;
+    // Если поддержка есть - браузер покажет заглушку - dummy.svg
+    // надо заменить заглушку на реальные изображения
+    const dataSrcsetElements = document.querySelectorAll('[data-srcset]');
+    dataSrcsetElements.forEach((dataSrcsetElement) => {
+      dataSrcsetElement.srcset = dataSrcsetElement.dataset.srcset;
+      delete dataSrcsetElement.dataset.srcset;
     });
-
-    const images = document.querySelectorAll('[data-src]');
-    images.forEach((img) => {
-      img.src = img.dataset.src;
-      delete img.dataset.src;
-    });
-
     nativeLazyloadIsEnabled = true;
-
   } else {
-
-    const lazyImages = document.querySelectorAll('[loading = "lazy"]');
-    lazyImages.forEach(lazyImage => {
-      lazyImage.classList.add('lazyload');
+    const lazyElements = document.querySelectorAll('[loading="lazy"]');
+    lazyElements.forEach((lazyElement) => {
+      lazyElement.classList.add('lazyload');
     });
-
-    const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js';
-    document.head.append(script);
-
-    nativeLazyloadIsEnabled = false;
-
+    appendLazysizesScript();
   }
 
   return { nativeLazyloadIsEnabled };
