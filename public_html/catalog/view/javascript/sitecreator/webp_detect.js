96036e11237028bf86df37d8e78a9d5c338f614d
Yulms <yminsk@gmail.com>
2021-02-19 09:25:21 +0300

    Удаляет комментарии из скрипта компрессор Sitecreator

--- public_html/catalog/view/javascript/sitecreator/webp_detect.js
+++ public_html/catalog/view/javascript/sitecreator/webp_detect.js
@@ -1,17 +1,13 @@
-
-// WEBP detect by sitecreator (c) 2019 https://sitecreator.ru webp_detect.js ver. 2.1.1
-// Code Developer Malyutin R. A. All rights reserved.
 (function() {
   if(typeof (window.sitecreator_hasWebP) !== 'object') window.sitecreator_hasWebP = {val: null};
 
   var usA = navigator.userAgent;
   var s;
   if(usA.match(/windows|android/i) !== null) if((s = usA.match(/(Chrome|Firefox)\/(\d{2,3})\./i)) !== null) {
-    // console.log(s);
     var br = s[1].toLowerCase();
     var ver = s[2];
     if((br === "chrome" &&   ver >= 32) || br === "firefox" && ver >= 65) {
-      window.sitecreator_hasWebP.val = true; // сработает если еще нет куки
+      window.sitecreator_hasWebP.val = true;
       console.log('webp on start= ok');
     }
   }
@@ -25,7 +21,6 @@
     window.sitecreator_hasWebP.val = false;
     console.log('webp = bad');
   };
-  // работает асинхроннно. в Хроме сработает сразу и до DOMContentLoaded, в FF - в конце (после DOMContentLoaded)
   img.onload = function() {
     if (img.width === 2 && img.height === 1) {
       document.cookie = "sitecreator_hasWebP=1; path=/";
@@ -38,29 +33,22 @@
 
 
 function funWebpOrNot2(tag, n) {
-  // console.log('funWebpOrNot2, document.readyState= ' + document.readyState);
-  // var me = document.currentScript;  // будет null если тег <script> добвлен динамически после document.readyState === complete
   if(typeof n == "undefined" || n === null) return;
   var me = document.getElementById('scwebp' + n);
-  if (me === null) return;  // null - если элемент не существует. выходим без отображения картинки
+  if (me === null) return;
 
-  // перестраховка, т.к. id уже уникальный в каждый отрезок времени
-  // на случай динамического создания на стр. <script>. Для одного запроса по http исключено дублирование id, да и js не выполняются параллельно
   if(typeof me.removeAttribute) me.removeAttribute('id');
 
-  if ((typeof (window.sitecreator_hasWebP) === 'undefined' || !window.sitecreator_hasWebP.val)) { // not webp
+  if ((typeof (window.sitecreator_hasWebP) === 'undefined' || !window.sitecreator_hasWebP.val)) {
     tag = tag.replace(/\.webp(['"\s])/g, '$1');
   }
 
   if(document.readyState === 'loading') {
     document.write(tag);
-    // удалим узел чтобы не мешал (чему, где  и когда?) удаление - лишняя операция и перестроение дерева DOM
-    //  подстраховка на гипотетический случай 2-го запуска одного и того же блока <script>, что невозможно после удаления его id, который к тому же уникален
-    if (typeof me.remove === 'function') me.remove(); // старые браузеры не знают
-    me = null; //отправляется к сборщику мусора
+    if (typeof me.remove === 'function') me.remove();
+    me = null;
   }
-  else me.insertAdjacentHTML("afterend", tag);  // метод поддерживается всеми зверями
-
+  else me.insertAdjacentHTML("afterend", tag);
 }
 
 function funWebpOrNot22(v) {
@@ -69,23 +57,16 @@ function funWebpOrNot22(v) {
   }
 }
 
-// на случай если в ускорителе включена оптимизация JS и все скрипты перемещаются в конец страницы
 function funWebpOrNot3(tag, n) {
-  // console.log('funWebpOrNot2, document.readyState= ' + document.readyState);
-  // var me = document.currentScript;  // будет null если тег <script> добвлен динамически после document.readyState === complete
   if(typeof n == "undefined" || n === null) return;
-  var me = document.getElementById('scwebp' + n); //
-  var ns = document.getElementById('nswebp' + n); // элемент <noscript>
-  if (me === null || ns === null) return;  // null - если элемент не существует. выходим без отображения картинки
+  var me = document.getElementById('scwebp' + n);
+  var ns = document.getElementById('nswebp' + n);
+  if (me === null || ns === null) return;
 
-  // перестраховка, т.к. id уже уникальный в каждый отрезок времени
-  // на случай динамического создания на стр. <script>. Для одного запроса по http исключено дублирование id, да и js не выполняются параллельно
-  // if(typeof me.removeAttribute) me.removeAttribute('id');
-
-  if ((typeof (window.sitecreator_hasWebP) === 'undefined' || !window.sitecreator_hasWebP.val)) { // not webp
+  if ((typeof (window.sitecreator_hasWebP) === 'undefined' || !window.sitecreator_hasWebP.val)) {
     tag = tag.replace(/\.webp(['"\s])/g, '$1');
   }
-  ns.insertAdjacentHTML("afterend", tag);  // метод поддерживается всеми зверями
+  ns.insertAdjacentHTML("afterend", tag);
 }
 
 function funWebpOrNot33(v) {
